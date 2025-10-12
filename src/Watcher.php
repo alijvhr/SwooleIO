e39abd308bb454952aab84daa583cdce3e433157
Alijvhr <eng.alijvhr@gmail.com>
2025-10-12 12:45:14 +0330

    refactor: improve file watching logic and add error handling in Watcher class

--- src/Watcher.php
+++ src/Watcher.php
@@ -6,6 +6,7 @@ use DirectoryIterator;
 use SplFileInfo;
 use Swoole\Event as SwEvent;
 use SwooleIO\Lib\Builder;
+use SwooleIO\Time\Timer;
 use Throwable;
 
 /**
@@ -25,12 +26,16 @@ class Watcher extends Builder
 
     public function start(): static
     {
-        $this->io = IO::instance();
+        $this->io ??= IO::instance();
         if ($this->paths && function_exists('inotify_init')) {
+            if (isset($this->inotify)) {
+                SwEvent::del($this->inotify);
+            }
             $this->inotify = inotify_init();
             foreach ($this->paths as $path) {
-                $this->io->log->info("Adding $path to hot reload paths");
-                $this->watch($path);
+                if ($this->watch($path)) {
+                    $this->io->log->info("Adding $path to hot reload paths");
+                }
             }
             SwEvent::add($this->inotify, $this->reload(...));
         }
@@ -48,15 +53,20 @@ class Watcher extends Builder
     protected function reload(): static
     {
         $changes = inotify_read($this->inotify);
-        if ($changes && !io()->timers->active('reload')) {
+        if ($changes && !$this->io->reloading) {
+            $this->io->reload(false, 1);
+            Timer::after(1, $this->start(...));
             $this->io->log->info('File modification detected. reloading...');
-            $this->io->timers->after('reload', 1, io()->reload(...));
         }
         return $this;
     }
 
-    protected function watch(string $path): static
+    protected function watch(string $path): bool
     {
+        if (!file_exists($path)) {
+            $this->io->log->warning("Path $path not found");
+            return false;
+        }
         inotify_add_watch($this->inotify, $path, IN_MODIFY | IN_CREATE);
         if (is_dir($path)) {
             try {
@@ -70,8 +80,9 @@ class Watcher extends Builder
                 }
             } catch (Throwable $e) {
                 $this->io->log->error("Failed to watch directory $path: " . $e->getMessage());
+                return false;
             }
         }
-        return $this;
+        return true;
     }
 }
